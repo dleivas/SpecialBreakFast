4b7e86c1003c57bf674cfba03737e92c165ffdcf
Diego Leivas <diego@gmail.com>
2015-11-12 09:10:15 -0500

    1

---
+++ SpecialBreakFast/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpecialBreakFast.MvcApplication" Language="C#" %>
---
+++ SpecialBreakFast/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpecialBreakFast.Startup))]
+namespace SpecialBreakFast
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
